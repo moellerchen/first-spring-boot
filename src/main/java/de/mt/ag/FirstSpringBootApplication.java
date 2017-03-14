ef11a608c80dbcb0cb46d9e76d21d189e6fa65ec
Möller, Catharina <catharina.moeller@mt-ag.com>
2017-03-14 10:30:48 +0100

    Initial commit

---
+++ src/main/java/de/mt/ag/FirstSpringBootApplication.java
@@ -0,0 +1,27 @@
+package de.mt.ag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+@SpringBootApplication
+public class FirstSpringBootApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+	
+	@RequestMapping("/test")
+	String test() {
+		return "Hello Docker World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootApplication.class, args);
+	}
+}
